7150e79c2052161338fd7bb132190b940594d0a9
Rustam Zak <rostik055@gmail.com>
2018-05-23 18:47:16 +0400

    okay

--- RZToDoListApp/RZAccount.cs
+++ RZToDoListApp/RZAccount.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 
--- RZToDoListApp/RZTasks.cs
+++ RZToDoListApp/RZTasks.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace RZToDoListApp
 {
+    [DataContract]
     class RZTasks
     {
+        [DataMember]
         public int RZTitle { get; set; }
+        [DataMember]
         public bool RZDone { get; set; }
+        [DataMember]
         public DateTime RZDateCreate { get; set; }
+        [DataMember]
         public DateTime RZDateClose { get; set; }
+        [DataMember]
+        public string RZUser { get; set; }
     }
 }
